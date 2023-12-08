451cf689c112de49b105b3f1fb35f39128ac4798
yogesh.nandanwar@capgemini.com <yogesh.nandanwar@capgemini.com>
2023-12-08 19:37:49 +0000

    jj

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,1 +1,9 @@
-select orderid as order_id,amount from  RAW.STRIPE.PAYMENT
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
